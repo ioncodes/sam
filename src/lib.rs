a220bf46add28385bc88a0bc329f6cd355cd1e65
Luca Marcelli <ionix@protonmail.com>
2018-01-19 17:53:16 +0100

    init

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+extern crate reqwest;
+
+#[macro_export]
+macro_rules! sam {
+    ($e:expr) => {
+        {
+            let client = reqwest::Client::new();
+            let mut res = client.post("http://localhost:1337/asm")
+                .body($e)
+                .send()
+                .unwrap();
+            let body = res.text().unwrap();
+            let mut asm = Vec::<u8>::new();
+            for opcode in body.split(",") {
+                asm.push(u8::from_str_radix(&opcode[2..], 16).unwrap());
+            }
+            asm
+        }
+    };
+}
