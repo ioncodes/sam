83fac2563d92bd711e3385a3a40feeae717be47b
Luca Marcelli <ionix@protonmail.com>
2018-01-20 13:36:05 +0100

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate reqwest;
 macro_rules! sam {
     ($e:expr) => {
         {
-            let client = ::reqwest::Client::new();
+            let client = self::reqwest::Client::new();
             let mut res = client.post("http://localhost:1337/asm")
                 .body($e)
                 .send()
