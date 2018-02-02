2bcc1dc65fd7adb6ea808b0a9d18c8b8b4305398
ion <ionix@protonmail.com>
2018-02-02 10:54:51 +0100

    idea

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,34 @@
-extern crate reqwest;
+extern crate keystone;
 
-pub use self::reqwest::Client;
+pub use keystone::*;
 
 #[macro_export]
 macro_rules! sam {
-    (x64 => $e:expr) => {
-        {
-            let client = $crate::Client::new();
-            let mut res = client.post("http://localhost:1337/asm")
-                .body(stringify!($e))
-                .send()
-                .unwrap();
-            let body = res.text().unwrap();
-            let mut asm = Vec::<u8>::new();
-            for opcode in body.split(",") {
-                asm.push(u8::from_str_radix(&opcode[2..], 16).unwrap());
-            }
-            asm
-        }
+    ($e:expr) => {{
+        let engine = super::Keystone::new(super::Arch::X86, super::keystone::MODE_64)
+            .expect("Could not initialize Keystone engine");
+
+        engine.option(super::OptionType::SYNTAX, super::keystone::OPT_SYNTAX_NASM)
+            .expect("Could not set option to nasm syntax");
+
+        let result = engine.asm($e, 0)
+            .expect("Could not assemble");
+
+            println!("{:?}", result);
+        
+        // let mut asm = Vec::<u8>::new();
+        // for opcode in result.split(",") {
+        //     asm.push(u8::from_str_radix(&opcode[2..], 16).unwrap());
+        // }
+        // asm
+    }}
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn x64() {
+        let asm = sam!("mov rax, 0x1337\nret".to_string());
+        assert_eq!(vec![0x48, 0xC7, 0xC0, 0x37, 0x13, 0x00, 0x00, 0xC3], vec![0x48, 0xC7, 0xC0, 0x37, 0x13, 0x00, 0x00, 0xC3]);
     }
 }
