9eda0236ae372cb2bf8d7bb120bdf203d71f6655
ion <ionix@protonmail.com>
2018-02-02 11:01:37 +0100

    new way

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,29 @@
-extern crate reqwest;
-
-pub use self::reqwest::Client;
-
 #[macro_export]
 macro_rules! sam {
-    (x64 => $e:expr) => {
+    ($e:expr) => {
         {
-            let client = $crate::Client::new();
-            let mut res = client.post("http://localhost:1337/asm")
-                .body(stringify!($e))
-                .send()
-                .unwrap();
-            let body = res.text().unwrap();
+            let output = if cfg!(target_os = "windows") {
+                    Command::new("cmd")
+                        .args(&["/C", "kstool", "x64nasm", $e])
+                        .output()
+                        .expect("failed to execute process")
+                } else {
+                    Command::new("sh")
+                        .arg("-c")
+                        .arg("kstool")
+                        .arg("x64nasm")
+                        .arg($e)
+                        .output()
+                        .expect("failed to execute process")
+            };
+            let result = &String::from_utf8(output.stdout).unwrap();
+            let out = result.split("[")[1].split("]")[0].replace(" ", "");
             let mut asm = Vec::<u8>::new();
-            for opcode in body.split(",") {
+            for opcode in out.split(",") {
                 asm.push(u8::from_str_radix(&opcode[2..], 16).unwrap());
             }
             asm
         }
     }
 }
+
