79209f3dbff152b9b3985bee42c3bb28f88ffdbf
ion <ionix@protonmail.com>
2018-02-05 18:24:55 +0100

    move things around

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(test)]
-
 pub mod mode;
 
 pub use std::process::Command;
@@ -53,43 +51,4 @@ macro_rules! sam {
     (x86 => $e:expr) => {{
         $crate::assemble($crate::Mode::X86, $e)
     }};
-}
-
-#[cfg(test)]
-mod tests {
-    extern crate test;
-
-    use self::test::Bencher;
-
-    #[test]
-    fn x64() {
-        let asm = sam!(x64 =>
-            "
-            mov rax, 0x1337
-            ret
-        "
-        );
-        assert_eq!(asm, vec![0x48, 0xC7, 0xC0, 0x37, 0x13, 0x00, 0x00, 0xC3]);
-    }
-
-    #[test]
-    fn x86() {
-        let asm = sam!(x86 =>
-            "
-            mov eax, 0x1337
-            ret
-        "
-        );
-        assert_eq!(asm, vec![0xb8, 0x37, 0x13, 0x00, 0x00, 0xC3]);
-    }
-
-    #[bench]
-    fn x64_bench(b: &mut Bencher) {
-        b.iter(|| x64());
-    }
-
-    #[bench]
-    fn x86_bench(b: &mut Bencher) {
-        b.iter(|| x86());
-    }
 }
