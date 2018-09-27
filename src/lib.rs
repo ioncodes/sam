a92aceb998ef46fe8a5d92611bb62e842b4abd18
Luca <luca.marcelli@fhnw.ch>
2018-09-27 09:01:03 +0200

    upgrade to new version of keystone-rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,40 @@
 extern crate keystone;
 
-pub use keystone::*;
-pub use keystone::gen::*;
+use keystone::*;
+
+pub fn asm(e: &'static str, arch: Arch, mode: Mode) -> Vec<u8> {
+    let engine = Keystone::new(arch, mode)
+        .expect("Could not initialize Keystone engine");
+
+    engine.option(OptionType::Syntax, OptionValue::SyntaxIntel)
+        .expect("Could not set option to nasm syntax");
+
+    let result = engine.asm(e, 0x0)
+        .expect("Could not assemble");
+
+    let mut buf = Vec::<u8>::new();
+    for str_byte in result.to_string().split(" ") {
+        buf.push(u8::from_str_radix(str_byte, 16).unwrap());
+    }
+
+    buf
+}
 
 #[macro_export]
 macro_rules! sam {
     (x64 => $e:expr) => {{
-        let engine = Keystone::new(KS_ARCH_X86, KS_MODE_64)
-            .expect("Could not initialize Keystone engine");
-
-        engine.option(KS_OPT_SYNTAX, KS_OPT_SYNTAX_INTEL)
-            .expect("Could not set option to nasm syntax");
-
-        let result = engine.asm($e, 0x0)
-            .expect("Could not assemble");
-        
-        let mut buf = Vec::<u8>::new();
-        for str_byte in result.to_string().split(" ") {
-            buf.push(u8::from_str_radix(str_byte, 16).unwrap());
-        }
-
-        buf
+        use keystone::{Arch, Mode};
+        $crate::asm($e, Arch::X86, Mode::Bit64)
     }};
 
-
     (x86 => $e:expr) => {{
-        let engine = Keystone::new(KS_ARCH_X86, KS_MODE_32)
-            .expect("Could not initialize Keystone engine");
-
-        engine.option(KS_OPT_SYNTAX, KS_OPT_SYNTAX_INTEL)
-            .expect("Could not set option to nasm syntax");
-
-        let result = engine.asm($e, 0x0)
-            .expect("Could not assemble");
-        
-        let mut buf = Vec::<u8>::new();
-        for str_byte in result.to_string().split(" ") {
-            buf.push(u8::from_str_radix(str_byte, 16).unwrap());
-        }
-
-        buf
+        use keystone::{Arch, Mode};
+        $crate::asm($e, Arch::X86, Mode::Bit32)
     }}
 }
 
 #[cfg(test)]
 mod tests {
-    use keystone::*;
-    use keystone::gen::*;
-
     #[test]
     fn x64() {
         let asm = sam!(x64 => "mov rax, 0x1337\nret");
@@ -56,7 +43,7 @@ mod tests {
 
     #[test]
     fn x86() {
-        let asm = sam!(x64 => "mov eax, 0x1337\nret");
+        let asm = sam!(x86 => "mov eax, 0x1337\nret");
         assert_eq!(asm, vec![0xb8, 0x37, 0x13, 0x00, 0x00, 0xc3]);
     }
 }
